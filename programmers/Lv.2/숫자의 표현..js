0da78aab87081f1c7cff473c8854dd0cee10adb6
LEE WOOJIN <24864@naver.com>
2024-10-18 13:13:55 +0900

    Create 숫자의 표현..js

---
+++ programmers/Lv.2/숫자의 표현..js
@@ -0,0 +1,15 @@
+function solution(n) {
+  var answer = 0;
+
+  for (let i = 1; i <= n; i++) {
+    let sum = i;
+    for (let j = i + 1; sum < n; j++) {
+      sum += j;
+    }
+    if (sum == n) {
+      answer++;
+    }
+  }
+
+  return answer;
+}
